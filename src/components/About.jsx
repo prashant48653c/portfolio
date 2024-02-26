76638253c703d1a51c219ec6384fdee143dedca2
Prashant <acharyaprashant227@gmail.com>
2024-02-26 18:17:46 +0545

    Some changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+ 
 import React, { useState, useEffect } from 'react';
 import moment from 'moment-timezone';
 
@@ -6,6 +7,7 @@ const About = () => {
   const [isActive, setIsActive] = useState(false);
   const [isStudy, setIsStudy] = useState(false);
   const [isSleep, setIsSleep] = useState(false);
+  const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
     const checkTime = () => {
@@ -37,59 +39,67 @@ const About = () => {
   }, []);
 
 
+  const handleClient = () => {
+    if (isClient) {
+      setIsClient(false)
 
+    } else {
+      setIsClient(true)
+    }
+  }
 
 
 
   return (
     <div className='myself'>
       <div className="mode-btns">
-      <button className='mode-btn' >🔥 Default</button>
-        
-        <button className='mode-btn' >🌙 Chill</button>
-       
-     
+        <button className='mode-btn' >🔥 Recruiter</button>
+
+        <button  onClick={handleClient} className='mode-btn' >🌙 Client</button>
+
+
 
       </div>
-    <div className='profile-img' >
-      {
-        isSleep && <img className='p-image' src='https://media.tenor.com/ecfWy7xK3PsAAAAM/sleep-zzz.gif' alt='sleep mode' />
-      }
-      {
-        isActive && <img className='p-image' src="https://i.pinimg.com/originals/38/a0/ef/38a0eff19955310e41a4d54dc1dd1838.gif" alt="" />
-      }
-      {
-        isStudy && <img className='p-image' src='https://i.pinimg.com/originals/b6/39/25/b63925166998c85d8b06f190c47c0e02.gif' />
-      }
+      <div className='profile-img' >
+        {
+          isSleep && <img className='p-image' src='https://media.tenor.com/ecfWy7xK3PsAAAAM/sleep-zzz.gif' alt='sleep mode' />
+        }
+        {
+          isActive && <img className='p-image' src="https://i.pinimg.com/originals/38/a0/ef/38a0eff19955310e41a4d54dc1dd1838.gif" alt="" />
+        }
+        {
+          isStudy && <img className='p-image' src='https://i.pinimg.com/originals/b6/39/25/b63925166998c85d8b06f190c47c0e02.gif' />
+        }
 
-<div style={{
-  backgroundColor: isActive ? "#0dff00" : (isStudy ? "#191970" : (isSleep ? "#ccc" : "transparent")),
-}} className="badge"></div>
+        <div style={{
+          backgroundColor: isActive ? "#0dff00" : (isStudy ? "#191970" : (isSleep ? "#ccc" : "transparent")),
+        }} className="badge"></div>
 
-</div>
+      </div>
 
-<div className="name">
- 
-    <h1>Prashant </h1>
-   <ul>
-    <li>💻 Web developer</li>
+      <div className="name">
 
-    <li>📱 Mobile app developer</li>
-    <li>🧑 Coding Teacher</li>
-    <li>👉Available for projects/work</li>
+        <h1>Prashant </h1>
+        <ul>
+
+          <li>💻 Web developer</li>
+
+          <li>📱 Mobile app developer</li>
+          <li>🧑 Coding Teacher</li>
+          <li>👉Available for projects/work</li>
 
 
- 
-   </ul>
 
-   <div>
-    <button className="btn">Hire me</button>
-    <button className="btn-resume">Resume</button>
+        </ul>
 
-   </div>
-</div>
+        <div>
+          <button className="btn">Hire me</button>
+          <button className="btn-resume"><a style={{color:"white"}}  href="https://drive.google.com/file/d/1v7x5Vmk5Lq0Ar0t8DPrQyi0j6ArksS_W/view" target='_blank' >Resume</a></button>
+
+        </div>
+      </div>
     </div>
-    
+
   )
 }
 
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,15 +1,60 @@
+ 
 import React from 'react'
 
 const Description = () => {
+
+  let projects=[
+    {
+      name:"GufSuff 💬",
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio temporibus facere, quia delectus sunt molestias sit excepturi tenetur nobis deleniti.",
+      img:"https://creatorspace.imgix.net/sites/ogimages/aHR0cHM6Ly9taXJvLm1lZGl1bS5jb20vdjIvcmVzaXplOmZpdDoxMjAwLzEqUmNJLWdobUhoLVBjNEpERGVua01BZy5qcGVn.jpeg?width=600&height=600"
+    },
+    {
+      name:"Streamzone 💬",
+      desc:"Developed Streamzone, a YouTube clone, using Material UI for UI, Axios for YouTube API integration, and Redux Toolkit for state management. Completed in 2079.",
+      img:"https://creatorspace.imgix.net/sites/ogimages/aHR0cHM6Ly9taXJvLm1lZGl1bS5jb20vdjIvcmVzaXplOmZpdDoxMjAwLzEqUmNJLWdobUhoLVBjNEpERGVua01BZy5qcGVn.jpeg?width=600&height=600"
+    },
+    {
+      name:"DexGame 💬",
+      desc:"Developed DexGames, a game store web app, with Chakra UI for UI, Axios for RAWG API integration, Redux Toolkit for state management, and Firebase for user authentication.",
+      img:"https://creatorspace.imgix.net/sites/ogimages/aHR0cHM6Ly9taXJvLm1lZGl1bS5jb20vdjIvcmVzaXplOmZpdDoxMjAwLzEqUmNJLWdobUhoLVBjNEpERGVua01BZy5qcGVn.jpeg?width=600&height=600"
+    },
+    {
+      name:"DayThought 💬",
+      desc:"Developed DayThought, a blogging web app, utilizing Material UI for responsiveness. Created APIs and endpoints, managed state and ensured security with JWT authentication",
+      img:"https://creatorspace.imgix.net/sites/ogimages/aHR0cHM6Ly9taXJvLm1lZGl1bS5jb20vdjIvcmVzaXplOmZpdDoxMjAwLzEqUmNJLWdobUhoLVBjNEpERGVua01BZy5qcGVn.jpeg?width=600&height=600"
+    },
+  ]
   return (
     <div className='d-boxs'>
       <h4>🎯 Projects</h4>
 
-      <div className="d-box">
-       <h6> Gufsuff 💬</h6>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio temporibus facere, quia delectus sunt molestias sit excepturi tenetur nobis deleniti.</p>
-       <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/chat-app-icon-logo-design-template-770ca6add87165646ba67d1a36dfee4e_screen.jpg?ts=1600190289" alt="" />
+<div className="d-box-container">
+
+{
+  projects.map((project,i)=>{
+    return (
+<div className="d-box">
+       <h6> {project.name} </h6>
+       <p>{project.desc}</p>
+       <img src={project.img} alt="" />
       </div>
+    )
+  })
+}
+
+
+    
+
+ 
+
+       
+
+</div>
+     
+
+
+
     </div>
   )
 }
