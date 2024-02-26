faf039dca2f9c2751e17702b0376dc64e8a4d9e9
Prashant <acharyaprashant227@gmail.com>
2024-02-26 08:03:48 +0545

    Some progress on about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment-timezone';
 
 const About = () => {
+
+  const [isActive, setIsActive] = useState(false);
+  const [isStudy, setIsStudy] = useState(false);
+  const [isSleep, setIsSleep] = useState(false);
+
+  useEffect(() => {
+    const checkTime = () => {
+      const nepalTime = moment().tz('Asia/Kathmandu');
+      const hour = nepalTime.hour();
+
+      if ((hour >= 6 && hour < 9) || (hour >= 18 && hour < 22)) {
+        setIsActive(true);
+        setIsStudy(false);
+        setIsSleep(false);
+      } else if (hour >= 9 && hour < 18) {
+        setIsActive(false);
+        setIsStudy(true);
+        setIsSleep(false);
+      } else {
+        setIsActive(false);
+        setIsStudy(false);
+        setIsSleep(true);
+      }
+    };
+
+    checkTime(); // Initial check
+
+    // Update every minute
+    const interval = setInterval(checkTime, 60000);
+
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+
+
+
   return (
     <div className='myself'>
+      <div className="mode-btns">
+      <button className='mode-btn' >🔥 Default</button>
+        
+        <button className='mode-btn' >🌙 Chill</button>
+       
+     
+
+      </div>
     <div className='profile-img' >
-<img src="https://creatorspace.imgix.net/users/cll9gp3rd074zmx01p9wg3qcn/LaPEIKZgp4IK91wm-IMG-3783.JPG?w=300&h=300" alt="" />
+      {
+        isSleep && <img className='p-image' src='https://media.tenor.com/ecfWy7xK3PsAAAAM/sleep-zzz.gif' alt='sleep mode' />
+      }
+      {
+        isActive && <img className='p-image' src="https://i.pinimg.com/originals/38/a0/ef/38a0eff19955310e41a4d54dc1dd1838.gif" alt="" />
+      }
+      {
+        isStudy && <img className='p-image' src='https://i.pinimg.com/originals/b6/39/25/b63925166998c85d8b06f190c47c0e02.gif' />
+      }
+
+<div style={{
+  backgroundColor: isActive ? "#0dff00" : (isStudy ? "#191970" : (isSleep ? "#ccc" : "transparent")),
+}} className="badge"></div>
+
 </div>
 
 <div className="name">
-    <h1>Prashant</h1>
+ 
+    <h1>Prashant </h1>
    <ul>
     <li>💻 Web developer</li>
 
@@ -22,6 +84,8 @@ const About = () => {
 
    <div>
     <button className="btn">Hire me</button>
+    <button className="btn-resume">Resume</button>
+
    </div>
 </div>
     </div>
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Description = () => {
   return (
-    <div className='d-box'>
+    <div className='d-boxs'>
+      <h4>🎯 Projects</h4>
 
+      <div className="d-box">
+       <h6> Gufsuff 💬</h6>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio temporibus facere, quia delectus sunt molestias sit excepturi tenetur nobis deleniti.</p>
+       <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/chat-app-icon-logo-design-template-770ca6add87165646ba67d1a36dfee4e_screen.jpg?ts=1600190289" alt="" />
+      </div>
     </div>
   )
 }
